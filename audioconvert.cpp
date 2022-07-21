12b41ce19675c157ede74f9065752571dc3243d1
Tursi <tacobell@harmlesslion.com>
2022-07-21 01:29:57 -0600

    - some more audio experiments - none worked

--- audioconvert.cpp
+++ audioconvert.cpp
@@ -18,10 +18,19 @@ unsigned char *wavein, *waveout;
 // I've added the missing 0 and changed the second 127 to 125, but this is untested...
 // (Confirmed that tools are using the entire range, except the missing index 9, which was the second 127)
 int sms_volume_table[16]={
-	255,191,159,143,135,131,129,
+#if 0
+    // hacky 2 bit instead of 4 bit version
+    255,255,255,255,128,128,128,
+	128,	//50,
+	65,65,65,65,0,0,0,0		// bottom half steps faster than top to try and equalize it
+
+#else
+
+    255,191,159,143,135,131,129,
 	128,	//50,
 	127,125,121,113,97,65,30,0		// bottom half steps faster than top to try and equalize it
 //      ^^^                 ^^   new entries, untested, todo above
+#endif
 
 //	240,215,190,170,155,140,129,
 //	128,	//50,
@@ -38,21 +47,126 @@ int maplevel(int nTmp) {
 	int nBest = -1;
 	int nDistance = INT_MAX;
 	int idx;
-	int nLast = 0x0f;
 
 	// testing says this is marginally better than just (nTmp>>4)
-	// The hand-tuned volume table helps a lot, but I wonder if we
+	// The hand-tuned volume table helps a /lot/, but I wonder if we
 	// could make that better? There's a lot of quantize error going
 	// down to 4 bit even if the logarithmic scale wasn't there, though.
-	for (idx=0; idx < 16; idx++) {
+#if 0
+    // this one discards the negative from the input wave and makes it all positive
+    // that results in very corrupted waveforms, regardless of mapping
+    if ((nTmp >= 126) && (nTmp <= 130)) {
+        nTmp = 0;
+    } else if (nTmp > 128) {
+        nTmp = (nTmp-128) * 2;
+    } else {
+        nTmp = (128-nTmp)*2;
+    }
+    for (idx=0; idx < 16; idx++) {
+		if (abs(real_sms_volume_table[idx]-nTmp) < nDistance) {
+			nBest = idx;
+			nDistance = abs(real_sms_volume_table[idx]-nTmp);
+		}
+	}
+#elif 0
+    // this is like the above, but fully discards the negative part of the waveform,
+    // looking only at the positive. The audio is recognizable, but clearly corrupted
+    if ((nTmp >= 126) && (nTmp <= 130)) {
+        nTmp = 0;
+    } else if (nTmp > 128) {
+        nTmp = (nTmp-128) * 2;
+    } else {
+        // negative
+        nTmp = 0;
+    }
+    for (idx=0; idx < 16; idx++) {
+		if (abs(real_sms_volume_table[idx]-nTmp) < nDistance) {
+			nBest = idx;
+			nDistance = abs(real_sms_volume_table[idx]-nTmp);
+		}
+	}
+#elif 0
+    // Similar to the first one, but when the waveform changes sign,
+    // we drop to zero for one frame
+    // Adds a rather strange distortion to the sound
+    static int nLast = 0;
+    if ((nTmp >= 126) && (nTmp <= 130)) {
+        nTmp = 0;
+        nLast = 0;
+    } else if (nTmp > 128) {
+        if (nLast >= 0) {
+            nLast = nTmp - 128;
+            nTmp = (nTmp-128) * 2;
+        } else {
+            nTmp = 0;
+            nLast = 0;
+        }
+    } else {
+        // negative
+        if (nLast <= 0) {
+            nLast = nTmp - 128;
+            nTmp = (128-nTmp)*2;
+        } else {
+            nTmp = 0;
+            nLast = 0;
+        }
+    }
+    for (idx=0; idx < 16; idx++) {
+		if (abs(real_sms_volume_table[idx]-nTmp) < nDistance) {
+			nBest = idx;
+			nDistance = abs(real_sms_volume_table[idx]-nTmp);
+		}
+	}
+
+#elif 1
+    // this is the best result so far, it uses a hand-tuned volume table scaled for counts around center
+    for (idx=0; idx < 16; idx++) {
 		if (abs(sms_volume_table[idx]-nTmp) < nDistance) {
 			nBest = idx;
 			nDistance = abs(sms_volume_table[idx]-nTmp);
 		}
 	}
+#elif 0
+    // this version attempts to map only the magnitude of the change
+    // this uses the real table. However, the output is pure noise with
+    // barely a semblance of the original
+    static int lastVal = -1;
+    static int lastOut = -1;
+
+    if (lastVal == -1) {
+        // just choose a relative starting point
+        for (idx=0; idx < 16; idx++) {
+		    if (abs(sms_volume_table[idx]-nTmp) < nDistance) {
+			    nBest = idx;
+			    nDistance = abs(real_sms_volume_table[idx]-nTmp);
+		    }
+	    }
+    } else {
+        // find the closest difference
+        int delta = abs(lastVal - nTmp);    // desired delta
+        for (int idx=0; idx<16; idx++) {
+            int tst = abs(real_sms_volume_table[idx] - real_sms_volume_table[lastOut]);
+            int dst = abs(tst-delta);
+            if (dst < nDistance) {
+                nBest = idx;
+                nDistance = dst;
+            }
+        }
+    }
+    lastVal = nTmp;
+    lastOut = nBest;
+
+#else
+    // this uses the real volume table and only works on very few samples
+    for (idx=0; idx < 16; idx++) {
+		if (abs(real_sms_volume_table[idx]-nTmp) < nDistance) {
+			nBest = idx;
+			nDistance = abs(real_sms_volume_table[idx]-nTmp);
+		}
+	}
+#endif
 
 	// return the index of the best match
-	nLast=nBest;
 	return nBest;
 }
 
@@ -151,17 +265,23 @@ int main(int argc, char* argv[])
 		printf("Can't open output file\n");
 		return -1;
 	}
-	// emulate high frequency output
-	int sign = 1;
-	// audio is 13485Hz, so let's just do a 4 times that for frequency - 53940Hz
-	// I think the chip can even pull that off... it's close anyway.
+#if 0
+	// emulate square wave output at 2mult times input frequency
+    int mult = 16;
+	for (int idx=0; idx<outpos; idx++) {
+		int outHi = real_sms_volume_table[waveout[idx]&0x0f]>>1;
+		int outLo = real_sms_volume_table[15-(waveout[idx]&0x0f)]>>1;
+        for (int j=0; j<mult; ++j) {
+            fputc(outHi, fp);
+      		fputc(outLo, fp);
+        }
+	}
+#else
 	for (int idx=0; idx<outpos; idx++) {
 		int out = real_sms_volume_table[waveout[idx]&0x0f]>>1;
 		fputc(out, fp);
-//		fputc(out, fp);
-//		fputc(-out, fp);
-//		fputc(-out, fp);
 	}
+#endif
 	fclose(fp);
 
 	free(wavein);
